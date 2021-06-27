d3674859271fe43e1f774673e8f392ddb4539b6e
faith2306 <chufofaithchioma@gmail.com>
2021-06-27 18:02:20 +0700

    initial commit

---
+++ build/precache-manifest.942f372499ca6ac6fcd4ce76371f5e69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab03534c8c984bcb5ba02f7b8d8eeafe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81c0ccdaa7e27befbdbe",
+    "url": "/static/js/2.2ed59e34.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/static/js/2.2ed59e34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9a4abcf14747fe34678",
+    "url": "/static/js/main.0b264e24.chunk.js"
+  },
+  {
+    "revision": "ed8ed874e871633992a1",
+    "url": "/static/js/runtime-main.a418149b.js"
+  },
+  {
+    "revision": "71e7aa76cb1fdfacc913a0268878aa1a",
+    "url": "/static/media/commerce.71e7aa76.png"
+  }
+]);
